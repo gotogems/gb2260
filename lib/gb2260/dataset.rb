a38c1777eb2a04cebd05c562d6b939329032c4fc
ichtum <65535758+ichtum@users.noreply.github.com>
2024-04-23 19:23:51 +0800

    ichtum created a commit :sparkles:

--- lib/gb2260/dataset.rb
+++ lib/gb2260/dataset.rb
@@ -1,22 +1,28 @@
 module GB2260
   class Dataset
+    extend Utils
     @@divisions = {}
-
-    def self.keys
-      @@divisions.keys
-    end
 
     def self.[](code)
       {
         code: code,
-        name: @@divisions[code]
+        name: @@divisions[code.to_s]
       }
+    end
+
+    def self.import(csv_file)
+      data = load_file(csv_file)
+      @@divisions.merge!(data.to_h)
     end
 
     def self.divisions
       keys.map do |division|
         Division.new(division)
       end
+    end
+
+    def self.keys
+      @@divisions.keys
     end
   end
 end
